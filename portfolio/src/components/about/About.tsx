3977380263ba355bd9eac6e4803576b416f2dff0
Gra <yvesb@live.be>
2022-03-13 16:07:22 +0100

    About component

--- portfolio/src/components/about/About.tsx
+++ portfolio/src/components/about/About.tsx
@@ -1,8 +1,48 @@
 import React from "react";
 import "./about.css";
-
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import gan from "../../assets/astolf/gan.png";
 function About() {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={gan} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Stagiar</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>yes</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+</small>
+            </article>
+          </div>
+          <p>Lorem Ispum</p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
